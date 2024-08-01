522147c9bf349cd81349bc32782940af4a09d84e
KyloChen <kylo.chen@tpisoftware.com>
2024-08-01 18:52:20 +0800

    調整新增全新狀態操作

--- src/app/components/diagram/diagram.component.ts
+++ src/app/components/diagram/diagram.component.ts
@@ -22,7 +22,6 @@ import type { ImportDoneEvent, ImportXMLResult } from 'bpmn-js';
  *                to navigate them
  * bpmn-modeler - bootstraps a full-fledged BPMN editor
  */
-import BpmnJS from 'bpmn-js/lib/Modeler';
 import { from, Observable, Subscription } from 'rxjs';
 
 import {
@@ -39,14 +38,11 @@ import { DiagramService } from 'src/app/service/diagram.service';
   templateUrl: './diagram.component.html',
   styleUrl: './diagram.component.scss',
 })
-export class DiagramComponent
-  implements AfterContentInit, OnChanges, OnDestroy, OnInit
-{
+export class DiagramComponent implements AfterContentInit, OnDestroy, OnInit {
   @ViewChild('ref', { static: true }) private el!: ElementRef;
   @ViewChild('propertiesPanel', { static: true })
   private propertiesPanel!: ElementRef;
 
-  @Input() public url?: string;
   @Output() private importDone: EventEmitter<ImportDoneEvent> =
     new EventEmitter();
   private bpmnModeler: BpmnModeler;
@@ -87,48 +83,25 @@ export class DiagramComponent
     if (this.diagramService.diagram()) {
       this.importDiagram(this.diagramService.diagram());
       return;
-    }
-
-    if (this.url) {
-      this.loadUrl(this.url);
+    } else {
+      /**全新狀態 */
+      this.loadDefaulXML();
     }
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // re-import whenever the url changes
-    if (changes['url']) {
-      this.loadUrl(changes['url'].currentValue);
-    }
+  loadDefaulXML(): void {
+    this.http.get('assets/default.xml', { responseType: 'text' }).subscribe(
+      (data) => {
+        this.importDiagram(data);
+      },
+      (error) => {
+        console.error('Failed to load XML file', error);
+      }
+    );
   }
 
   ngOnDestroy(): void {
     this.bpmnModeler.destroy();
-  }
-
-  /**
-   * Load diagram from URL and emit completion event
-   */
-  loadUrl(url: string): Subscription {
-    return this.http
-      .get(url, { responseType: 'text' })
-      .pipe(
-        switchMap((xml: string) => this.importDiagram(xml)),
-        map((result) => result.warnings)
-      )
-      .subscribe(
-        (warnings) => {
-          this.importDone.emit({
-            type: 'success',
-            warnings,
-          });
-        },
-        (err) => {
-          this.importDone.emit({
-            type: 'error',
-            error: err,
-          });
-        }
-      );
   }
 
   /**
